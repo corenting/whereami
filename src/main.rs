0a643090aa5254229fdc53eb4bf8eddb828f9db4
corenting <corenting@gmail.com>
2016-05-10 19:51:23 +0100

    License & readme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/*
+whereami, a console application to get your location using freegeoip.net
+Copyright (C) 2016  corenting
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 extern crate hyper;
 extern crate rustc_serialize;
 extern crate getopts;
